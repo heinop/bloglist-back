c6641ccec3c9caca4fc1650ef138eb714190df17
heinop <pekka.heino@unikie.com>
2020-09-18 11:25:10 +0300

    Task 4.2 Divided application to modules

--- index.js
+++ index.js
@@ -1,59 +1,10 @@
-const config = require('./utils/config');
 const http = require('http');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const app = require('./app');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
 
-mongoose.set('useFindAndModify', false);
+const server = http.createServer(app);
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  }
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('connected to MongoDB');
-  })
-  .catch(error => {
-    console.error('error connecting to MongoDB:', error.message);
-  });
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result);
-    });
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
